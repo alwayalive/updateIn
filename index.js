05e94115aa323c81a32bdb5472da1047f22f80fd
alwayalive <alwayalive@163.com>
2017-08-08 15:24:57 +0800

    compile code

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-export default function updateIn(o, ns, v) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = updateIn;
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function updateIn(o, ns, v) {
     if (!ns) return null;
-    !Array.isArray(ns) && (ns = [ns])
+    !Array.isArray(ns) && (ns = [ns]);
 
     function _updateIn(_o) {
-        let tn = ns.shift();
+        var tn = ns.shift();
         if (Array.isArray(_o)) {
-            let __o = _o.slice();
-            __o[tn] = ns.length ? _updateIn(__o[tn]) : v
-            return __o
-        } else return Object.assign({}, _o, {
-            [tn]: (ns.length ? _updateIn(_o[tn]) : v)
-        })
+            var __o = _o.slice();
+            __o[tn] = ns.length ? _updateIn(__o[tn]) : v;
+            return __o;
+        } else return Object.assign({}, _o, _defineProperty({}, tn, ns.length ? _updateIn(_o[tn]) : v));
     }
     return _updateIn(o);
 }
